67be689a68ffb6b945ab27468bbf26fb641a34b1
unknown <nathanmahesa36@gmail.com>
2024-10-06 23:35:57 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#2563eb',
+        secondary: '#64748b',
+        dark: '#0f172a',
+        light: '#f8fafc'
+      },
+      screens: {
+        '2xl' : '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
+
